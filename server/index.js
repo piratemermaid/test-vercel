eadf625ae24500edb7d9b6d4eb4f0f9ca2929dbc
piratemermaid <thepiratemermaid4@gmail.com>
2023-12-19 11:36:46 -0700

    Add test server response

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", (_req, res) => {
+    const testResponse = fs.readFileSync("server/response.json");
+    res.send(JSON.parse(testResponse));
 });
 
 app.listen(port, () => {
